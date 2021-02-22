ce544193bfdedba7241f1e5200ba83d0a3f9c0ca
schini <hyuio0228@gmail.com>
2021-02-22 23:46:19 +0900

    up to 06

--- 06 Longest Palindrome Substring.py
+++ 06 Longest Palindrome Substring.py
@@ -6,13 +6,14 @@ test_case_2 = "abcdddd"
 
 
 # 가장 긴 palindrome string 찾기
-def longest_palindrome_substring_my(string: str) -> str:
+def longest_palindrome_my(string: str) -> str:
     length = len(string)
-    max_longest = 1
-    longest_palindrome = string[0]
+    max_longest = 1  # 가장 긴 palindrome 철자 개수
+    longest_palindrome = string[0]  # 가장 긴 palindrome
     i = 1
 
-    while i < length - (max_longest + 1) / 2:  # 홀수 개 탐색
+    # 홀수 개 탐색
+    while i < length - (max_longest + 1) / 2:
         j = 1
         while i - j >= 0 and i + j < length:
             if string[i - j] != string[i + j]:
@@ -26,7 +27,8 @@ def longest_palindrome_substring_my(string: str) -> str:
 
     i = 1
 
-    while i < length - (max_longest - 1) / 2:  # 짝수 개 탐색
+    # 짝수 개 탐색
+    while i < length - (max_longest - 1) / 2:
         j = 1
         if string[i - 1] == string[i]:
             while i - j >= 1 and i + j < length:
@@ -42,9 +44,31 @@ def longest_palindrome_substring_my(string: str) -> str:
     return longest_palindrome
 
 
-print(longest_palindrome_substring_my(test_case_1))
-print(longest_palindrome_substring_my(test_case_2))
+print(longest_palindrome_my(test_case_1))  # 'abadaba'
+print(longest_palindrome_my(test_case_2))  # 'dddd'
 
 
-def longest_palindrome_substring_sol1():
-    return 1
+def longest_palindrome_sol1(s: str) -> str:
+    # two pointer
+    def expand(left: int, right: int) -> str:
+        while left >= 0 and right <= len(s) and s[left] == s[right - 1]:
+            left -= 1
+            right += 1
+        return s[left + 1:right - 1]
+
+    # 특이 케이스 빠르게 리턴
+    if len(s) < 2 or s == s[::-1]:
+        return s
+
+    result = ''
+    # sliding window 이동
+    for i in range(len(s) - 1):
+        result = max(result,
+                     expand(i, i + 1),  # 홀수 개 경우
+                     expand(i, i + 2),  # 짝수 개 경우
+                     key=len)
+    return result
+
+
+print(longest_palindrome_sol1(test_case_1))  # 'abadaba'
+print(longest_palindrome_sol1(test_case_2))  # 'dddd'
